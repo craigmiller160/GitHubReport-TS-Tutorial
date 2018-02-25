8bfc438da8f01431f0f6450a5b2b3465ac6c08c1
Craig Miller <craigmiller160@gmail.com>
2018-02-25 13:12:01 -0500

    Commented out unused code

--- src/service/githubapi.ts
+++ src/service/githubapi.ts
@@ -3,29 +3,29 @@ import {User} from '../model/user';
 import {Repo} from '../model/repo';
 import {Promise} from 'es6-promise';
 
+// export class GitHubApiService2 {
+//
+//     getUserInfo(userName: string, callback: (user: User) => any){
+//         request.get(`https://api.github.com/users/${userName}`, OPTIONS, (error: any, res: any, body: any) => {
+//             let user: User = new User(body);
+//             callback(user);
+//         });
+//     }
+//
+//     getRepos(userName: string, callback: (repoArray: Repo[]) => any){
+//         request.get(`https://api.github.com/users/${userName}/repos`, OPTIONS, (error: any, res: any, body: any) => {
+//             let repoArray: Repo[] = body.map((repo: any) => new Repo(repo));
+//             callback(repoArray);
+//         });
+//     }
+// }
+
 const OPTIONS: any = {
     headers: {
         'User-Agent': 'request'
     },
     json: true
 };
-
-export class GitHubApiService2 {
-
-    getUserInfo(userName: string, callback: (user: User) => any){
-        request.get(`https://api.github.com/users/${userName}`, OPTIONS, (error: any, res: any, body: any) => {
-            let user: User = new User(body);
-            callback(user);
-        });
-    }
-
-    getRepos(userName: string, callback: (repoArray: Repo[]) => any){
-        request.get(`https://api.github.com/users/${userName}/repos`, OPTIONS, (error: any, res: any, body: any) => {
-            let repoArray: Repo[] = body.map((repo: any) => new Repo(repo));
-            callback(repoArray);
-        });
-    }
-}
 
 export class GitHubApiService {
 
